13e8aa97a8b7f1aa84d29905836470089e269e38
danielclough <danielclough@gmail.com>
2020-04-12 19:42:17 -0700

    update helmet

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import Helmet from 'react-helmet'
+import { Helmet } from 'react-helmet'
 import { DiscussionEmbed } from "disqus-react"
 
 import { Layout } from '../components/common'
